8ba2619ba8729a8d0e077da6dfae4a27d60e82cc
Yangfan Zhang <42492049+YangfanZhang@users.noreply.github.com>
2020-07-14 21:57:24 +1000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,35 @@ function App() {
     email: ""
   });
 
+  function handleChange(event) {
+    const input = event.target;
+    const name = input.name;
+    setContact(prevValue => {
+      switch (name) {
+        case "fName":
+          return {
+            fName: input.value,
+            lName: prevValue.lName,
+            email: prevValue.email
+          };
+        case "lName":
+          return {
+            fName: prevValue.fName,
+            lName: input.value,
+            email: prevValue.email
+          };
+        case "email":
+          return {
+            fName: prevValue.fName,
+            lName: prevValue.lName,
+            email: input.value
+          };
+        default:
+          break;
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +43,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={contact.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
